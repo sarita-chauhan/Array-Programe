0a8176750e47dc6fa333f0234efc4ee0d3a392d4
Sarita Chauhan <124147666+sarita-chauhan@users.noreply.github.com>
2023-03-23 01:09:53 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Array2{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int size=sc.nextInt();
+        int number []=new int [size];
+        for(int i=0;i<size; i++){
+            System.out.println(number[i]);
+        }
+        
+    }
+}
