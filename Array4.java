18da5bf107b1da319c77316fc90fa182654f77fc
Sarita Chauhan <124147666+sarita-chauhan@users.noreply.github.com>
2023-03-23 01:10:35 +0530

    Add files via upload

---
+++ Array4.java
@@ -0,0 +1,10 @@
+public class Array4 {
+    public static void main(String[] args) {
+        int a[]={2,4,6,8};
+        int sum=0;
+        for(int i=0;i<4;i++){
+            sum=sum+a[i];
+        }
+        System.out.println(sum);
+    }
+}
