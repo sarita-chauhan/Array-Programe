40c083d6bf0e0dc8db955e634e1dbd7ba681588c
Sarita Chauhan <124147666+sarita-chauhan@users.noreply.github.com>
2023-03-30 19:59:15 +0530

    Add files via upload

---
+++ KthSmallestElement.java
@@ -0,0 +1,25 @@
+public class KthSmallestElement {
+    public static void main(String[] args) {
+        
+        int[] a={5,8,12,7,6,2,4 ,10,21,33};
+        
+          
+        int k=4;
+      for(int i=0;i<a.length;i++){
+
+          for(int j=i+1;j<a.length;j++){
+
+              if(a[i]>a[j]){
+                  int temp=a[i];
+                  a[i]=a[j];
+                  a[j]=temp;
+              }
+          }
+          if(i==k-1){
+              System.out.println(k+ "smallest elementis "+a[i]);
+              break;
+      }
+
+  }
+    }
+}
